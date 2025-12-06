b26bdc6d320975b1877a5130080bccd1261c44f9
officialleslar <144202158+lestikejora-official@users.noreply.github.com>
2025-12-06 12:07:40 +0700

    Commit changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { MessageCircle } from 'lucide-react';
 
 function App() {
   const handleWhatsAppClick = () => {
-    const phoneNumber = '6283151089786';
+    const phoneNumber = '6285176999928';
     const message = encodeURIComponent('Hallo, CS ADAKAMI');
     window.open(`https://wa.me/${phoneNumber}?text=${message}`, '_blank');
   };
